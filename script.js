c41a163ab4ab1514432ac42c87ee161aa2b52778
wongpakho814 <wongpakho814@gmail.com>
2023-05-13 21:20:49 +1000

    Implemented scaling effect to changing sections

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const menu = document.querySelector(".menu");
 
 menu.addEventListener("click", () => {
-  document.querySelector(".navbar").classList.toggle("change");  
+  document.querySelector(".navbar").classList.toggle("change");
 });
 // End of menu icon
 
@@ -14,6 +14,11 @@ let bool = true;
 const sections = document.querySelectorAll("section");
 const progress = document.querySelector(".progress h2");
 const circles = document.querySelectorAll(".circle");
+
+const section1wrapper = document.querySelector(".section-1-wrapper");
+const section5wrapper = document.querySelector(".section-5-wrapper");
+
+section1wrapper.style.transform = "scale(1)";
 
 const progressCounter = () => {
   progress.textContent = `${counter2}/${sections.length}`;
@@ -32,6 +37,8 @@ const pageController = () => {
     });
     counter1 = 0;
     counter2 = 1;
+    section1wrapper.style.transform = "scale(1)";
+    section5wrapper.style.transform = "scale(1.5)";
     progressCounter();
     bool = false;
   }
@@ -45,6 +52,8 @@ const pageController = () => {
     });
     counter1 = 4;
     counter2 = 5;
+    section1wrapper.style.transform = "scale(1.5)";
+    section5wrapper.style.transform = "scale(1)";
     progressCounter();
     bool = false;
   }
@@ -66,9 +75,19 @@ window.addEventListener("wheel", (event) => {
 
   pageController();
 
-  bool && (document.querySelector(
-    `.section-${deltaY ? counter1 : counter2}`
-  ).style.left = `${deltaY ? "-100vw" : "0"}`);
+  if (bool) {
+    document.querySelector(
+      `.section-${deltaY ? counter1 : counter2}`
+    ).style.left = `${deltaY ? "-100vw" : "0"}`;
+
+    document.querySelector(
+      `.section-${deltaY ? counter1 : counter2}-wrapper`
+    ).style.transform = `scale(${deltaY ? "1.5" : "1"})`;
+
+    document.querySelector(
+      `.section-${deltaY ? counter1 + 1 : counter2 + 1}-wrapper`
+    ).style.transform = `scale(${deltaY ? "1" : "1.5"})`;
+  }
 });
 // End of scroll effect and progress bar
 
@@ -76,13 +95,29 @@ window.addEventListener("wheel", (event) => {
 document.querySelector(".left-btn").addEventListener("click", () => {
   counter1--;
   counter2--;
-  pageController() && (document.querySelector(`.section-${counter2}`).style.left = "0");
+  pageController() &&
+    (document.querySelector(`.section-${counter2}`).style.left = "0");
+
+  if (bool) {
+    document.querySelector(`.section-${counter2}-wrapper`).style.transform =
+      "scale(1)";
+    document.querySelector(`.section-${counter2 + 1}-wrapper`).style.transform =
+      "scale(1.5)";
+  }
 });
 
 document.querySelector(".right-btn").addEventListener("click", () => {
   counter1++;
   counter2++;
-  pageController() && (document.querySelector(`.section-${counter1}`).style.left = "-100vw");
+  pageController() &&
+    (document.querySelector(`.section-${counter1}`).style.left = "-100vw");
+
+  if (bool) {
+    document.querySelector(`.section-${counter2}-wrapper`).style.transform =
+      "scale(1)";
+    document.querySelector(`.section-${counter1}-wrapper`).style.transform =
+      "scale(1.5)";
+  }
 });
 
 document.querySelector(".grapes-img").addEventListener("mouseover", () => {
