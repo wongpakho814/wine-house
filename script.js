4d387ff1d80be2da10246b4d931a7ccccff683e9
wongpakho814 <wongpakho814@gmail.com>
2023-05-13 20:07:28 +1000

    Implemented JS functionality to page buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Scroll effect
+// Scroll effect and progress bar
 let counter1 = 0;
 let counter2 = 1;
+let bool = true;
 
 const sections = document.querySelectorAll("section");
 const progress = document.querySelector(".progress h2");
@@ -14,16 +15,8 @@ const progressCounter = () => {
   document.querySelector(`.circle-${counter2}`).style.backgroundColor = "#ddd";
 };
 
-window.addEventListener("wheel", (event) => {
-  const deltaY = event.deltaY > 0;
-
-  if (deltaY) {
-    counter1++;
-    counter2++;
-  } else {
-    counter1--;
-    counter2--;
-  }
+const pageController = () => {
+  bool = true;
 
   if (counter1 === sections.length) {
     sections.forEach((section) => {
@@ -32,7 +25,7 @@ window.addEventListener("wheel", (event) => {
     counter1 = 0;
     counter2 = 1;
     progressCounter();
-    return;
+    bool = false;
   }
 
   if (counter1 === -1) {
@@ -45,10 +38,50 @@ window.addEventListener("wheel", (event) => {
     counter1 = 4;
     counter2 = 5;
     progressCounter();
+    bool = false;
   }
 
   progressCounter();
+  return bool;
+};
 
-  document.querySelector(`.section-${deltaY ? counter1 : counter2}`).style.left = `${deltaY ? "-100vw" : "0"}`;
+window.addEventListener("wheel", (event) => {
+  const deltaY = event.deltaY > 0;
+
+  if (deltaY) {
+    counter1++;
+    counter2++;
+  } else {
+    counter1--;
+    counter2--;
+  }
+
+  pageController();
+
+  bool && (document.querySelector(
+    `.section-${deltaY ? counter1 : counter2}`
+  ).style.left = `${deltaY ? "-100vw" : "0"}`);
 });
-// End of scroll effect
+// End of scroll effect and progress bar
+
+// Page buttons
+document.querySelector(".left-btn").addEventListener("click", () => {
+  counter1--;
+  counter2--;
+  pageController() && (document.querySelector(`.section-${counter2}`).style.left = "0");
+});
+
+document.querySelector(".right-btn").addEventListener("click", () => {
+  counter1++;
+  counter2++;
+  pageController() && (document.querySelector(`.section-${counter1}`).style.left = "-100vw");
+});
+
+document.querySelector(".grapes-img").addEventListener("mouseover", () => {
+  document.querySelector(".section-3-wrapper").style.opacity = "0.5";
+});
+
+document.querySelector(".grapes-img").addEventListener("mouseout", () => {
+  document.querySelector(".section-3-wrapper").style.opacity = "1";
+});
+// End of page buttons
